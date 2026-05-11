1c454697cda6f456def48cfc7cd5f339f1fedff7
HeoYeon <mathofy@gmail.com>
2026-05-11 18:56:24 +0900

    fix: Add MathJax script to render LaTeX formulas in Blogger

--- scripts/blogger/generator.py
+++ scripts/blogger/generator.py
@@ -76,6 +76,22 @@ def generate_blog_post(topic, target_url):
         
         # 마크다운을 완벽한 HTML로 강제 변환
         html_body = markdown.markdown(body, extensions=['extra', 'sane_lists'])
+        
+        # Blogger에서 LaTeX 수식($$, \( 등)이 렌더링되도록 MathJax 스크립트를 본문 하단에 추가
+        mathjax_script = """
+<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+<script>
+MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\\\(', '\\\\)']],
+    displayMath: [['$$', '$$'], ['\\\\[', '\\\\]']],
+    processEscapes: true
+  }
+};
+</script>
+<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+"""
+        html_body += mathjax_script
 
         return title, html_body
 
