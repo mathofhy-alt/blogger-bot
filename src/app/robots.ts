55908dc835b829aa9a7ca253db0706d87f1b755b
HeoYeon <mathofy@gmail.com>
2026-05-13 01:02:20 +0900

    SEO: 네이버 Yeti 크롤러 명시적 허용 추가

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,7 +3,17 @@ import { SITE_CONFIG } from '@/data/config';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: '*', allow: '/' }],
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      // 네이버 검색로봇 명시적 허용
+      {
+        userAgent: 'Yeti',
+        allow: '/',
+      },
+    ],
     sitemap: `${SITE_CONFIG.url}/sitemap.xml`,
   };
 }
